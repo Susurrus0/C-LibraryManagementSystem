f4b33b59858e213a164a92fffdad2d18ba5ad729
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-20 22:25:16 +0100

    Add main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO Implement struct for books, with members: author, title, release date, genre, page count
+
+int main() {
+    // Your code here
+    
+    return 0;
+}
+
+// TODO Implement function for displaying a list of all books
+
+
+// TODO Implement function for adding a new book
+
+
+// TODO Implement function for deleting an existing book
+
+
+// TODO Implement function for borrowing books
+
+
+// TODO Implement function for displaying a list of all borrowed books
