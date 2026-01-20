29ee354f2cd4ae9441cad1c317f73667b1427583
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-20 23:35:16 +0100

    Implement listBooks(), add member id to struct book

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 // Struct for books, with members: author, title, release date, genre, page count.
 struct book {
+    int id;
     char author[32];
     char title[32];
     char releaseDate[16];
@@ -10,15 +11,32 @@ struct book {
     int pageCount;
 };
 
+void listBooks(struct book *library, int librarySize);
+
 int main() {
     struct book library[0];
+    int librarySize = sizeof(library) / sizeof(library[0]);
 
-    printf("===== Library Manager =====\n");
+    printf("======== Library Manager ========\n");
+    printf("** librarySize = %i\n", librarySize);
     
     return 0;
 }
 
-// TODO Implement function for displaying a list of all books
+// Function for displaying the list of all books.
+void listBooks(struct book *library, int librarySize) {
+    if (librarySize < 1) {
+        printf("There are no books in the Library.\n");
+        return;
+    }
+
+    printf("ID\tAuthor\tTitle\tRelease Date\tGenre\tPage Count\n");
+    for (int i = 0; i < librarySize; i++) {
+        printf("%i\t%s\t%s\t%s\t%s\t%i\n",
+            library[i].id, library[i].author, library[i].title,
+            library[i].releaseDate, library[i].genre, library[i].pageCount);
+    }
+}
 
 
 // TODO Implement function for adding a new book
