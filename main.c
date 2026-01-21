36f6b875bb4c67e683bd77988a2158c6b89efa55
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-21 19:23:01 +0100

    Implement addBook()

--- main.c
+++ main.c
@@ -28,25 +28,28 @@ int main() {
     printf("======== Library Manager ========\n");
     printf("\t** library.catalogueSize = %i\n", library.catalogueSize);
     
+
+    free(library.catalogue);
+    library.catalogue = NULL;
     return 0;
 }
 
-// Function for displaying the list of all books.
+// Function for displaying the list of all books in the Library's catalogue.
 void listBooks(Library library) {
     if (library.catalogueSize < 1) {
         printf("There are no books in the Library.\n");
         return;
     }
 
-    printf("Author\tTitle\tYear Released\tGenre\tPage Count\n");
+    printf("ID\tAuthor\tTitle\tYear Released\tGenre\tPage Count\n");
     for (int i = 0; i < library.catalogueSize; i++) {
-        printf("%s\t%s\t%i\t%s\t%i\n",
-            library.catalogue[i].author, library.catalogue[i].title, library.catalogue[i].yearReleased,
-            library.catalogue[i].genre, library.catalogue[i].pageCount);
+        printf("#%i\t%s\t%s\t%i\t%s\t%i\n",
+            i, library.catalogue[i].author, library.catalogue[i].title,
+            library.catalogue[i].yearReleased,library.catalogue[i].genre, library.catalogue[i].pageCount);
     }
 }
 
-// TODO Implement function for adding a new book
+// Function for adding a new book to the Library's catalogue.
 void addBook(Library library) {
     book newBook;
 
@@ -62,7 +65,11 @@ void addBook(Library library) {
     scanf("%i", newBook.pageCount);
 
     // Realloc library to fit 1 more book and add newBook to library
+    library.bookCount++;
+    library.catalogue = realloc(library.catalogue, sizeof(book) * library.bookCount);
+    library.catalogueSize = sizeof(library.catalogue) / sizeof(library.catalogue[0]);
 
+    library.catalogue[library.bookCount - 1] = newBook;
 }
 
 // TODO Implement function for deleting an existing book
