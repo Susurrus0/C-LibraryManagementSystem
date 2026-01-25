86e5a42f26f47d96a95cb69da5308471a4fe01e4
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-01-26 00:41:30 +0100

    Fix several issues, mostly with pointers

--- main.c
+++ main.c
@@ -14,62 +14,69 @@ typedef struct {
 typedef struct {
     int bookCount;
     book *catalogue;
-    int catalogueSize;
 } Library;
 
-void listBooks(Library library);
-void addBook(Library library);
+void listBooks(Library *library);
+void addBook(Library *library);
 
 int main() {
     Library library = {0};
-    library.catalogue = (book*) malloc(sizeof(book) * library.bookCount);
-    library.catalogueSize = sizeof(library.catalogue) / sizeof(library.catalogue[0]);
+    Library *libPtr = &library;
 
     printf("======== Library Manager ========\n");
-    printf("\t** library.catalogueSize = %i\n", library.catalogueSize);
+    printf("\t** libPtr->bookCount = %i\n", libPtr->bookCount);
+    printf("\t** sizeof(book) = %i\n", sizeof(book));
+    printf("\t** sizeof(Library) = %i\n", sizeof(Library));
     
+    addBook(libPtr);
 
-    free(library.catalogue);
-    library.catalogue = NULL;
+    printf("\t** libPtr->bookCount = %i\n", libPtr->bookCount);
+
+    listBooks(libPtr);
+
+    free(libPtr->catalogue);
+    libPtr->catalogue = NULL;
     return 0;
 }
 
 // Function for displaying the list of all books in the Library's catalogue.
-void listBooks(Library library) {
-    if (library.catalogueSize < 1) {
+void listBooks(Library *library) {
+    if (library->bookCount < 1) {
         printf("There are no books in the Library.\n");
         return;
     }
 
-    printf("ID\tAuthor\tTitle\tYear Released\tGenre\tPage Count\n");
-    for (int i = 0; i < library.catalogueSize; i++) {
-        printf("#%i\t%s\t%s\t%i\t%s\t%i\n",
-            i, library.catalogue[i].author, library.catalogue[i].title,
-            library.catalogue[i].yearReleased,library.catalogue[i].genre, library.catalogue[i].pageCount);
+    printf("ID\t Author\t\t Title\t\t Year Released\t Genre\t\t Page Count\n");
+    for (int i = 0; i < library->bookCount; i++) {
+        printf("#%i\t %s\t %s\t %i\t\t %s\t %i\n",
+            i, library->catalogue[i].author, library->catalogue[i].title,
+            library->catalogue[i].yearReleased, library->catalogue[i].genre, library->catalogue[i].pageCount);
     }
 }
 
 // Function for adding a new book to the Library's catalogue.
-void addBook(Library library) {
+void addBook(Library *library) {
     book newBook;
 
+    printf("Adding new book...\n");
     printf("Who is the author of the book? ");
     scanf("%s", newBook.author);
     printf("What is the book's title? ");
     scanf("%s", newBook.title);
     printf("What year was the book released in? ");
-    scanf("%i", newBook.yearReleased);
+    scanf("%d", &newBook.yearReleased);
     printf("What genre is the book part of? ");
     scanf("%s", newBook.genre);
     printf("How many pages does the book have? ");
-    scanf("%i", newBook.pageCount);
+    scanf("%d", &newBook.pageCount);
 
     // Realloc library to fit 1 more book and add newBook to library
-    library.bookCount++;
-    library.catalogue = realloc(library.catalogue, sizeof(book) * library.bookCount);
-    library.catalogueSize = sizeof(library.catalogue) / sizeof(library.catalogue[0]);
+    library->bookCount++;
+    library->catalogue = realloc(library->catalogue, sizeof(book) * library->bookCount);
 
-    library.catalogue[library.bookCount - 1] = newBook;
+    library->catalogue[library->bookCount - 1] = newBook;
+
+    printf("New book successfully added.\n");
 }
 
 // TODO Implement function for deleting an existing book
